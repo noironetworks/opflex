cc8c3889bb7d451d2f0301e35a2082504d765862
Rob Adams <readams@readams.net>
2018-02-28 13:20:12 -0800

    Don't set source MAC when routing to load balanced service.
    * Should help prevent problems in VLAN mode when using intermediate
      learning switch.
    
    Change-Id: I2cbb20f4539b17938a39de15f2ddfd5bc31fe0a1
    Signed-off-by: Rob Adams <readams@readams.net>

--- agent-ovs/src/IntFlowManager.cpp
+++ agent-ovs/src/IntFlowManager.cpp
@@ -1430,9 +1430,10 @@ void IntFlowManager::handleServiceUpdate(const string& uuid) {
 
                 serviceDest
                     .priority(50)
-                    .ipDst(serviceAddr)
-                    .action()
-                    .ethSrc(getRouterMacAddr());
+                    .ipDst(serviceAddr);
+                if (as.getServiceMode() == Service::LOCAL_ANYCAST) {
+                    serviceDest.action().ethSrc(getRouterMacAddr());
+                }
 
                 if (!nextHopAddrs.empty()) {
                     // map traffic to service to the next hop IPs
