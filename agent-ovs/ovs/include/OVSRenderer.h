b26b0e91eb983311fd8e0dcc46dc295c5c9ac26d
Kiran Shastri <shastrinator@gmail.com>
2019-08-30 15:52:24 -0700

    Send TunnelEp Advertisements
    Opflex-agent needs to send RARP/GARP requests
    for the uplink interface for physical switch to learn tunnel mac.
    Default behavior is to send RARP requests.
    Configurable parameter to choose GARP requests or
    to disable TunnelEp advertisment is added under renderer.
    forwarding:endpoint-advertisements:tunnel-endpoint-mode
    
    Signed-off-by: Kiran Shastri <shastrinator@gmail.com>
    Change-Id: I985242e30b3e36ea88040901b6c0129df2edda23

--- agent-ovs/ovs/include/OVSRenderer.h
+++ agent-ovs/ovs/include/OVSRenderer.h
@@ -65,6 +65,7 @@ private:
     FlowReader intFlowReader;
     PortMapper intPortMapper;
     SwitchManager intSwitchManager;
+    TunnelEpManager tunnelEpManager;
     IntFlowManager intFlowManager;
 
     FlowExecutor accessFlowExecutor;
@@ -78,7 +79,6 @@ private:
     InterfaceStatsManager interfaceStatsManager;
     ContractStatsManager contractStatsManager;
     SecGrpStatsManager secGrpStatsManager;
-    TunnelEpManager tunnelEpManager;
 
     std::string intBridgeName;
     std::string accessBridgeName;
@@ -92,6 +92,7 @@ private:
     std::string virtualRouterMac;
     bool routerAdv;
     AdvertManager::EndpointAdvMode endpointAdvMode;
+    AdvertManager::EndpointAdvMode tunnelEndpointAdvMode;
     bool virtualDHCP;
     std::string virtualDHCPMac;
     std::string flowIdCache;
