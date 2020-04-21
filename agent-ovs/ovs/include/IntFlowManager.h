594813ef88c5e14c3534547df0301b68072a89a5
gautvenk <gautam.chennai@gmail.com>
2020-04-20 22:45:25 -0700

    Prometheus optimization: Avoid exporting pod<-->svc Nan metrics
    - Added agent config knob to disable export of nan pod-svc metrics by default
    - During stats update, if pkt count is non-0, then metrics will get created
    - Metric will get created once, and future stats updates will just update metric val.
    - if config changes take place to ep or service, then annotations will get updated provided the metric was already created (due to non-0 stats)
    - not relying on obs mo stats for updating prom metrics. Whatever value is supplied by client is used.
    - Added make check tests for pod svc metrics using curl
    
    Signed-off-by: Gautam Venkataramanan <gautam.chennai@gmail.com>
    Change-Id: I7114a8fca979c0d74dbfdd8f2946a22bab57106e

--- agent-ovs/ovs/include/IntFlowManager.h
+++ agent-ovs/ovs/include/IntFlowManager.h
@@ -654,9 +654,7 @@ private:
                                    const bool &isEpToSvc,
                                    const string& idStr,
                                    const uint64_t &pkts,
-                                   const uint64_t &bytes,
-                                   const attr_map &ep_attr_map,
-                                   const attr_map &svc_attr_map);
+                                   const uint64_t &bytes);
 
     /*
      * Update svc counter objects
