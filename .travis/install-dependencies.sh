d00065c4f4e5c02f9860b4b4c141ea54e8f9f77b
gautvenk <gautam.chennai@gmail.com>
2020-05-15 16:30:33 -0700

    Travis build speedup: checking if cached dir is empty to proceed building dependencies
    Signed-off-by: Gautam Venkataramanan <gautam.chennai@gmail.com>

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -5,11 +5,17 @@ set -x
 
 mkdir -p ../ovs
 pushd ../ovs
-git clone https://github.com/openvswitch/ovs.git --branch v2.12.0 --depth 1
+if ! [ "$(ls -A .)" ]; then
+    git clone https://github.com/openvswitch/ovs.git --branch v2.12.0 --depth 1
+    pushd ovs
+    ./boot.sh &> /dev/null
+    ./configure  --enable-shared &> /dev/null
+    make -j2 &> /dev/null
+    popd
+else
+    echo "using cached ovs"
+fi
 pushd ovs
-./boot.sh &> /dev/null
-./configure  --enable-shared &> /dev/null
-make -j2 &> /dev/null
 sudo make install &> /dev/null
 sudo mkdir -p /usr/local/include/openvswitch/openvswitch
 sudo mv /usr/local/include/openvswitch/*.h /usr/local/include/openvswitch/openvswitch
@@ -21,17 +27,23 @@ popd
 
 mkdir -p ../prometheus-cpp
 pushd ../prometheus-cpp
-git clone https://github.com/noironetworks/3rdparty-debian.git
-cp 3rdparty-debian/prometheus/prometheus-cpp.patch ~
-git clone https://github.com/jupp0r/prometheus-cpp.git
+if ! [ "$(ls -A .)" ]; then
+    git clone https://github.com/noironetworks/3rdparty-debian.git
+    cp 3rdparty-debian/prometheus/prometheus-cpp.patch ~
+    git clone https://github.com/jupp0r/prometheus-cpp.git
+    pushd prometheus-cpp
+    git checkout 9effb90b0c266316358680cbf862a8564eb2c2d4
+    git submodule init
+    git submodule update
+    git apply ~/prometheus-cpp.patch
+    mkdir _build && cd _build
+    cmake .. -DBUILD_SHARED_LIBS=ON &> /dev/null
+    make $make_args
+    popd
+else
+    echo "using cached prometheus-cpp"
+fi
 pushd prometheus-cpp
-git checkout 9effb90b0c266316358680cbf862a8564eb2c2d4
-git submodule init
-git submodule update
-git apply ~/prometheus-cpp.patch
-mkdir _build && cd _build
-cmake .. -DBUILD_SHARED_LIBS=ON &> /dev/null
-make $make_args
 sudo make install
 popd
 popd
