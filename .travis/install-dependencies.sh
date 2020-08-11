27a5428e2bff6f373a496e4e240a78ea7be080e3
Gautam Venkataramanan <gautam.chennai@gmail.com>
2020-08-10 19:28:42 -0700

    Install cmake v3.17.0 and move to grpc v1.31.0 in travis
    Signed-off-by: Gautam Venkataramanan <gautam.chennai@gmail.com>

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -12,15 +12,15 @@ sudo dpkg -i prometheus-cpp_0.9.0_amd64.deb
 mkdir -p ../grpc
 pushd ../grpc
 if ! [ "$(ls -A .)" ]; then
-    git clone https://github.com/grpc/grpc
+    wget -q -O cmake-linux.sh https://github.com/Kitware/CMake/releases/download/v3.17.0/cmake-3.17.0-Linux-x86_64.sh
+    sh cmake-linux.sh -- --skip-license --prefix=/usr/local
+    rm cmake-linux.sh
+    git clone --recurse-submodules -b v1.31.0 https://github.com/grpc/grpc
     pushd grpc
-    git checkout 5052efd666ab6fdda2a4b3045569f70ce0c5fa57
-    git submodule update --init
-    make -j2
-    pushd third_party/protobuf
-    ./autogen.sh
-    ./configure
-    make -j2
+    mkdir -p cmake/build
+    pushd cmake/build
+    cmake -DgRPC_INSTALL=ON -DgRPC_BUILD_TESTS=OFF -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=/usr/local ../..
+    make -j4
     popd
     popd
 else
@@ -28,8 +28,5 @@ else
 fi
 pushd grpc
 sudo make install
-pushd third_party/protobuf
-sudo make install
-popd
 popd
 popd
