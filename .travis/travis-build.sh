bc7680fae8ef7ede61dbfb87c57ba28579fee0ab
Gautam Venkataramanan <gautam.chennai@gmail.com>
2020-08-13 16:39:25 -0700

    Fix Codacy warning

--- .travis/travis-build.sh
+++ .travis/travis-build.sh
@@ -35,7 +35,6 @@ popd
 popd
 
 pushd agent-ovs
-echo $LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/usr/local/lib
 ./autogen.sh &> /dev/null
 ./configure --enable-coverage --enable-gprof --enable-grpc &> /dev/null
