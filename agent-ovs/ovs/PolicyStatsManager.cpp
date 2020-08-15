bd29570a8aaf64086e69a0088c40bb8a01e0b675
Gautam Venkataramanan <gautam.chennai@gmail.com>
2020-08-15 10:57:38 -0700

    Update PolicyStatsManager.cpp
    Looks like a merge issue. Removing ret=false which is redundant.

--- agent-ovs/ovs/PolicyStatsManager.cpp
+++ agent-ovs/ovs/PolicyStatsManager.cpp
@@ -407,7 +407,6 @@ void PolicyStatsManager::handleMessage(int msgType,
         return;
     }
     if (msgType == OFPTYPE_FLOW_STATS_REPLY) {
-        bool ret = false;
         std::lock_guard<std::mutex> lock(pstatMtx);
         ofp_header *msgHdr = (ofp_header *)msg->data;
         ovs_be32 recvXid = msgHdr->xid;
