6c24a5fc1aa342c60593cad394188642bc7a7b3b
Madhu Challa <challa@gmail.com>
2023-08-09 16:11:38 -0700

    Fix jammy dependencies
    - add missing pc files
    - downgrade to openssl1.1, default being 3.0

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -3,10 +3,20 @@
 set -o errtrace
 set -x
 
+# Force openssl 1.1 on 22.04 systems, default is 3.0
+wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl/openssl_1.1.1f-1ubuntu2.19_amd64.deb
+wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl-dev_1.1.1f-1ubuntu2.19_amd64.deb
+wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.19_amd64.deb
+
+sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2.19_amd64.deb
+sudo dpkg -i libssl-dev_1.1.1f-1ubuntu2.19_amd64.deb
+sudo dpkg -i openssl_1.1.1f-1ubuntu2.19_amd64.deb
+
 wget https://travisci-static-artifacts-dd485362-9714-11ea-bb37-0242ac130002.s3.us-east-2.amazonaws.com/artifacts.tgz
 tar -xvzf artifacts.tgz
-#sudo dpkg -i libnoiro-openvswitch_2.12.0-1_amd64.deb
-#sudo dpkg -i libnoiro-openvswitch-dev_2.12.0-1_amd64.deb
+
+sudo dpkg -i libnoiro-openvswitch_2.12.0-1_amd64.deb
+sudo dpkg -i libnoiro-openvswitch-dev_2.12.0-1_amd64.deb
 sudo dpkg -i prometheus-cpp_1.0.1_amd64.deb
 
 mkdir -p ../grpc
@@ -30,8 +40,7 @@ fi
 pushd grpc/cmake/build
 sudo make install
 sudo cp ../../third_party/re2/re2.pc /usr/local/share/pkgconfig/
-sudo cp ../../cmake/build/libs/opt/pkgconfig/*.pc /usr/local/share/pkgconfig/
-sudo cp ../../cmake/build/third_party/zlib/zlib.pc /usr/local/share/pkgconfig/
-sudo cp ../../cmake/build/third_party/protobuf/*.pc /usr/local/share/pkgconfig/
+sudo cp third_party/protobuf/*.pc /usr/local/share/pkgconfig/
+sudo cp libs/opt/pkgconfig/*.pc /usr/local/share/pkgconfig/
 popd
 popd
