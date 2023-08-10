7865203fbdc8982bc90360905fb11d516b117dd5
anmol372 <anmrasto@cisco.com>
2023-08-09 18:04:50 -0700

    revert travis-build change

--- .travis/travis-build.sh
+++ .travis/travis-build.sh
@@ -37,7 +37,7 @@ popd
 pushd agent-ovs
 export LD_LIBRARY_PATH=/usr/local/lib
 ./autogen.sh &> /dev/null
-./configure --enable-coverage --enable-gprof --enable-grpc
+./configure --enable-coverage --enable-gprof --enable-grpc &> /dev/null
 make -j2
 sudo make install
 set +e
