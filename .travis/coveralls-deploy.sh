e9a2cca731376eb4d28f0bf9e0e7f50fdd899170
anmol372 <anmrasto@cisco.com>
2025-09-19 12:02:59 -0700

    wip

--- .travis/coveralls-deploy.sh
+++ .travis/coveralls-deploy.sh
@@ -1,8 +1,27 @@
-#!/bin/bash
-
 if [ "$TEST_SUITE" == "travis-build.sh" ]; then
-  lcov --capture --directory . --output-file coverage-all.info > /dev/null
-  lcov --remove coverage-all.info -o coverage-all.info '/usr/include/*' '/usr/local/include/*' '*/test/*' > /dev/null
-  export COVERALLS_SERVICE_NAME=travis-pro
-  coveralls --lcov-file coverage-all.info --exclude '/usr/include' --exclude '/usr/local/include' --exclude-pattern '.*/test/.*' --gcov-options '\-lp' > /dev/null
+  lcov --capture \
+       --directory . \
+       --base-directory "$(pwd)" \
+       --no-external \
+       --ignore-errors gcov,source \
+       --output-file coverage-all.info \
+       >/dev/null 2>/dev/null
+
+  #lcov --capture --directory . --output-file coverage-all.info >/dev/null 2>/dev/null
+  
+  lcov --remove coverage-all.info -o coverage-all.info \
+  '/usr/include/*' '/usr/local/include/*' '*/test/*' \
+  '*/cmd/gbp_inspect.cpp' '*/cmd/mcast_daemon.cpp' '*/cmd/opflex_agent.cpp' \
+  '*/cmd/integration-test/*' '*/debian/*' '*/rpm/*' '*/m4/*' '*/doc/*' \
+  '*/sample/*' '*/ovs/*' '*/autogen.sh' '*configure.ac' '*Makefile.am' '*.in' \
+  '*/Doxyfile*' >/dev/null 2>/dev/null 
+
+
+  coveralls --lcov-file coverage-all.info \
+    --exclude '/usr/include' \
+    --exclude '/usr/local/include' \
+    --exclude-pattern '.*/(test|integration-test|debian|rpm|m4|doc|sample|ovs)/.*' \
+    --exclude-pattern '.*/cmd/(gbp_inspect|mcast_daemon|opflex_agent)\.cpp' \
+    --exclude-pattern '.*(configure\.ac|Makefile\.am|\.in|Doxyfile.*)' \
+    --gcov-options '\-lp'
 fi
