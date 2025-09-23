16684b1512ca16872aa8f5cf8ce3016c3f00b311
anmol372 <anmrasto@cisco.com>
2025-09-23 11:24:32 -0700

    try1

--- .travis/coveralls-deploy.sh
+++ .travis/coveralls-deploy.sh
@@ -1,27 +1,35 @@
+#!/bin/bash
+
 if [ "$TEST_SUITE" == "travis-build.sh" ]; then
+  # Capture with early filtering - prevents processing unwanted files
   lcov --capture \
        --directory . \
        --base-directory "$(pwd)" \
        --no-external \
        --ignore-errors gcov,source \
+       --exclude '/usr/include/*' \
+       --exclude '/usr/local/include/*' \
+       --exclude '*/test/*' \
+       --exclude '*/cmd/gbp_inspect.cpp' \
+       --exclude '*/cmd/mcast_daemon.cpp' \
+       --exclude '*/cmd/opflex_agent.cpp' \
+       --exclude '*/cmd/integration-test/*' \
+       --exclude '*/debian/*' \
+       --exclude '*/rpm/*' \
+       --exclude '*/m4/*' \
+       --exclude '*/doc/*' \
+       --exclude '*/sample/*' \
+       --exclude '*/ovs/*' \
+       --exclude '*/autogen.sh' \
+       --exclude '*configure.ac' \
+       --exclude '*Makefile.am' \
+       --exclude '*.in' \
+       --exclude '*/Doxyfile*' \
        --output-file coverage-all.info \
-       >/dev/null 2>/dev/null
-
-  #lcov --capture --directory . --output-file coverage-all.info >/dev/null 2>/dev/null
-  
-  lcov --remove coverage-all.info -o coverage-all.info \
-  '/usr/include/*' '/usr/local/include/*' '*/test/*' \
-  '*/cmd/gbp_inspect.cpp' '*/cmd/mcast_daemon.cpp' '*/cmd/opflex_agent.cpp' \
-  '*/cmd/integration-test/*' '*/debian/*' '*/rpm/*' '*/m4/*' '*/doc/*' \
-  '*/sample/*' '*/ovs/*' '*/autogen.sh' '*configure.ac' '*Makefile.am' '*.in' \
-  '*/Doxyfile*' >/dev/null 2>/dev/null 
+       >/dev/null 2>&1
 
 
   coveralls --lcov-file coverage-all.info \
-    --exclude '/usr/include' \
-    --exclude '/usr/local/include' \
-    --exclude-pattern '.*/(test|integration-test|debian|rpm|m4|doc|sample|ovs)/.*' \
-    --exclude-pattern '.*/cmd/(gbp_inspect|mcast_daemon|opflex_agent)\.cpp' \
-    --exclude-pattern '.*(configure\.ac|Makefile\.am|\.in|Doxyfile.*)' \
-    --gcov-options '\-lp'
+    --gcov-options '\-lp' \
+    >/dev/null 2>&1
 fi
