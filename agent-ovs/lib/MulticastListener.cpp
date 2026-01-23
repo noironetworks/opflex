06352626151d73056c8e5ce28619332c5d414c38
Thomas Bachman <tbachman@yahoo.com>
2026-01-23 08:33:51 -0500

    Allow limiting multicast interfaces
    The multicast daemon (mcast_daemon) manages multicast membership
    using a socket to communicate to the kernel's IP stack which multicast
    groups should be joined or left. The kernel currently responds to
    multicast membership queries on any interface that receives it.
    
    This patch introduces a new runtime argument to the multicast
    daemon, which limits the interfaces that the will be used for
    responding to multicast membership queries. If no interface
    arguments are provided, then all interfaces will respond to
    queries. Multiple interfaces can be specified by passing multiple
    arguments when starting the agent.
    
    (cherry picked from commit 9189dfa9b0059adec3325f6d145e1c1a032a8e7c)

--- agent-ovs/lib/MulticastListener.cpp
+++ agent-ovs/lib/MulticastListener.cpp
@@ -16,6 +16,12 @@
 #include <boost/asio/ip/v6_only.hpp>
 #include <boost/asio/ip/multicast.hpp>
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <ifaddrs.h>
+#include <net/if.h>
+#include <arpa/inet.h>
+
 
 namespace opflexagent {
 
@@ -24,13 +30,80 @@ using std::shared_ptr;
 using std::unique_ptr;
 using std::unordered_set;
 using std::string;
+using std::vector;
 
 #define LISTEN_PORT 34242
 
-MulticastListener::MulticastListener(ba::io_service& io_service_)
+bool MulticastListener::resolveInterface(const std::string& ifname,
+                                         ba::ip::address_v4& addr_v4,
+                                         unsigned int& if_index) {
+    // Get interface index
+    if_index = if_nametoindex(ifname.c_str());
+    if (if_index == 0) {
+        LOG(ERROR) << "Interface not found: " << ifname;
+        return false;
+    }
+
+    // Get IPv4 address for the interface
+    struct ifaddrs *ifaddr, *ifa;
+    bool found_v4 = false;
+
+    if (getifaddrs(&ifaddr) == -1) {
+        LOG(ERROR) << "Failed to get interface addresses";
+        return false;
+    }
+
+    for (ifa = ifaddr; ifa != nullptr; ifa = ifa->ifa_next) {
+        if (ifa->ifa_addr == nullptr)
+            continue;
+
+        if (ifname == ifa->ifa_name && ifa->ifa_addr->sa_family == AF_INET) {
+            struct sockaddr_in *sin = (struct sockaddr_in *)ifa->ifa_addr;
+            char ip_str[INET_ADDRSTRLEN];
+            inet_ntop(AF_INET, &sin->sin_addr, ip_str, sizeof(ip_str));
+            boost::system::error_code ec;
+            addr_v4 = ba::ip::address_v4::from_string(ip_str, ec);
+            if (!ec) {
+                found_v4 = true;
+                break;
+            }
+        }
+    }
+
+    freeifaddrs(ifaddr);
+
+    if (!found_v4) {
+        LOG(WARNING) << "No IPv4 address found for interface: " << ifname
+                     << " (IPv4 multicast will not be available on this interface)";
+    }
+
+    return true;  // Return true if we got the interface index (IPv6 will still work)
+}
+
+MulticastListener::MulticastListener(ba::io_service& io_service_,
+                                     const vector<string>& interfaces)
     : io_service(io_service_), running(true) {
 
     LOG(INFO) << "Starting multicast listener";
+
+    // Resolve interfaces to addresses/indices if specified
+    if (!interfaces.empty()) {
+        LOG(INFO) << "Restricting multicast membership to specified interfaces";
+        for (const auto& ifname : interfaces) {
+            ba::ip::address_v4 addr_v4;
+            unsigned int if_index = 0;
+            if (resolveInterface(ifname, addr_v4, if_index)) {
+                if (!addr_v4.is_unspecified()) {
+                    interface_addrs_v4.push_back(addr_v4);
+                    LOG(INFO) << "Added interface " << ifname
+                              << " with IPv4 address " << addr_v4;
+                }
+                interface_indices_v6.push_back(if_index);
+                LOG(INFO) << "Added interface " << ifname
+                          << " with index " << if_index << " for IPv6";
+            }
+        }
+    }
 
     try {
         unique_ptr<ba::ip::udp::socket> v4(new ba::ip::udp::socket(io_service));
@@ -108,21 +181,56 @@ void MulticastListener::join(const std::string& mcast_address) {
     LOG(INFO) << "Joining group " << addr;
 
     if (addr.is_v4()) {
-        if (socket_v4)
-            socket_v4->set_option(ba::ip::multicast::join_group(addr), ec);
-        else
+        if (!socket_v4) {
             LOG(ERROR) << "Could not join group "
                        << addr << ": " << "IPv4 socket not available";
+            return;
+        }
+
+        if (interface_addrs_v4.empty()) {
+            // No interface restriction - join on all interfaces
+            socket_v4->set_option(ba::ip::multicast::join_group(addr), ec);
+            if (ec)
+                LOG(ERROR) << "Could not join group " << addr << ": " << ec.message();
+        } else {
+            // Join on each specified interface
+            for (const auto& if_addr : interface_addrs_v4) {
+                socket_v4->set_option(
+                    ba::ip::multicast::join_group(addr.to_v4(), if_addr), ec);
+                if (ec) {
+                    LOG(ERROR) << "Could not join group " << addr
+                               << " on interface " << if_addr << ": " << ec.message();
+                } else {
+                    LOG(DEBUG) << "Joined group " << addr << " on interface " << if_addr;
+                }
+            }
+        }
     } else {
-        if (socket_v6)
-            socket_v6->set_option(ba::ip::multicast::join_group(addr), ec);
-        else
+        if (!socket_v6) {
             LOG(ERROR) << "Could not join group "
                        << addr << ": " << "IPv6 socket not available";
-    }
+            return;
+        }
 
-    if (ec)
-        LOG(ERROR) << "Could not join group " << addr << ": " << ec.message();
+        if (interface_indices_v6.empty()) {
+            // No interface restriction - join on all interfaces
+            socket_v6->set_option(ba::ip::multicast::join_group(addr), ec);
+            if (ec)
+                LOG(ERROR) << "Could not join group " << addr << ": " << ec.message();
+        } else {
+            // Join on each specified interface
+            for (unsigned int if_index : interface_indices_v6) {
+                socket_v6->set_option(
+                    ba::ip::multicast::join_group(addr.to_v6(), if_index), ec);
+                if (ec) {
+                    LOG(ERROR) << "Could not join group " << addr
+                               << " on interface index " << if_index << ": " << ec.message();
+                } else {
+                    LOG(DEBUG) << "Joined group " << addr << " on interface index " << if_index;
+                }
+            }
+        }
+    }
 }
 
 void MulticastListener::leave(const std::string& mcast_address) {
@@ -134,15 +242,50 @@ void MulticastListener::leave(const std::string& mcast_address) {
     LOG(INFO) << "Leaving group " << addr;
 
     if (addr.is_v4()) {
-        if (socket_v4)
-            socket_v4->set_option(ba::ip::multicast::leave_group(addr), ec);
-    } else {
-        if (socket_v6)
-            socket_v6->set_option(ba::ip::multicast::leave_group(addr), ec);
-    }
+        if (!socket_v4)
+            return;
 
-    if (ec)
-        LOG(ERROR) << "Could not leave group " << addr << ": " << ec.message();
+        if (interface_addrs_v4.empty()) {
+            // No interface restriction - leave on all interfaces
+            socket_v4->set_option(ba::ip::multicast::leave_group(addr), ec);
+            if (ec)
+                LOG(ERROR) << "Could not leave group " << addr << ": " << ec.message();
+        } else {
+            // Leave on each specified interface
+            for (const auto& if_addr : interface_addrs_v4) {
+                socket_v4->set_option(
+                    ba::ip::multicast::leave_group(addr.to_v4(), if_addr), ec);
+                if (ec) {
+                    LOG(ERROR) << "Could not leave group " << addr
+                               << " on interface " << if_addr << ": " << ec.message();
+                } else {
+                    LOG(DEBUG) << "Left group " << addr << " on interface " << if_addr;
+                }
+            }
+        }
+    } else {
+        if (!socket_v6)
+            return;
+
+        if (interface_indices_v6.empty()) {
+            // No interface restriction - leave on all interfaces
+            socket_v6->set_option(ba::ip::multicast::leave_group(addr), ec);
+            if (ec)
+                LOG(ERROR) << "Could not leave group " << addr << ": " << ec.message();
+        } else {
+            // Leave on each specified interface
+            for (unsigned int if_index : interface_indices_v6) {
+                socket_v6->set_option(
+                    ba::ip::multicast::leave_group(addr.to_v6(), if_index), ec);
+                if (ec) {
+                    LOG(ERROR) << "Could not leave group " << addr
+                               << " on interface index " << if_index << ": " << ec.message();
+                } else {
+                    LOG(DEBUG) << "Left group " << addr << " on interface index " << if_index;
+                }
+            }
+        }
+    }
 }
 
 void MulticastListener::sync(const shared_ptr<unordered_set<string> >& naddrs) {
